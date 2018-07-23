681179565956aaeed2cd188ec8d7b8e57b4a3567
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-23 17:16:56 +0200

    add replacementMissingValues

--- src/main/scala/fr/poverty/spark/utils/ReplacementNoneValuesTask.scala
+++ src/main/scala/fr/poverty/spark/utils/ReplacementNoneValuesTask.scala
@@ -7,12 +7,38 @@ class ReplacementNoneValuesTask(val labelColumn: String,
 
   def run(data: DataFrame): Unit = {
 
+    val meanData = computeMeanByColumns(data)
+    meanData.show()
+    meanData.printSchema()
+
+    replaceMissingValues(data, meanData, labelColumn, noneColumns)
+
   }
 
   def computeMeanByColumns(data: DataFrame): DataFrame = {
     var meanData = data.groupBy(labelColumn).mean(noneColumns.toSeq: _*)
     noneColumns.foreach(column => meanData = meanData.withColumnRenamed(s"avg($column)", s"$column"))
     meanData
+  }
+
+//  def getMeanValue(data: DataFrame, column: String) = {
+//    data.filter(col(column) == )
+//  }
+
+//  def getTargetValues(data: DataFrame, columnName: String): Array[Int] = {
+//    data.select(labelColumn).distinct().rdd.map(x => x.getInt(x.fieldIndex(columnName))).collect()
+//  }
+
+  def replaceMissingValues(data: DataFrame, meanData: DataFrame, labelColumn: String, noneColumns: Array[String]) = {
+    noneColumns.foreach(column => {
+      println(column)
+      val map = meanData.rdd.map(x => (x.getInt(x.fieldIndex(labelColumn)), x.getDouble(x.fieldIndex(column)))).collectAsMap()
+      println(column, map)
+    })
+
+
+
+
   }
 
 }
