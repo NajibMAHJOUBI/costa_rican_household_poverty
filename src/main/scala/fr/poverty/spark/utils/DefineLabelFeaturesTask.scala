64317f22764f02d07fc0317cec154b4bcc9beba9
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-24 17:30:07 +0200

    add method featuresVector

--- src/main/scala/fr/poverty/spark/utils/DefineLabelFeaturesTask.scala
+++ src/main/scala/fr/poverty/spark/utils/DefineLabelFeaturesTask.scala
@@ -1,8 +1,10 @@
 package fr.poverty.spark.utils
 
+import org.apache.spark.ml.linalg.{Vector, Vectors}
 import org.apache.spark.sql.DataFrame
 
 import scala.io.Source
+import scala.math.log
 
 class DefineLabelFeaturesTask(val labelColumn: String) {
 
@@ -17,6 +19,7 @@ class DefineLabelFeaturesTask(val labelColumn: String) {
     Source.fromFile("src/main/resources/featuresNames").getLines.toList(0).split(",")
   }
 
+  def featuresVector() = {}
 
 
 }
