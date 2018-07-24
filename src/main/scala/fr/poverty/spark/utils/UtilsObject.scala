89f0573bbfcdd1a6a9bbf6ac4869f48a78aa18ba
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-24 15:56:09 +0200

    add method fillDoubleValue, fillIntValue and defineMapMissingValues

--- src/main/scala/fr/poverty/spark/utils/UtilsObject.scala
+++ src/main/scala/fr/poverty/spark/utils/UtilsObject.scala
@@ -1,10 +1,21 @@
 package fr.poverty.spark.utils
 
+import org.apache.spark.sql.DataFrame
+
+import scala.collection.Map
+
 object UtilsObject {
 
-  def dealNullValue(x: Option[Double]): Option[Double] = {
-    val num = x.getOrElse(return None)
-    Some(num)
+  def defineMapMissingValues(data: DataFrame, labelCol: String, column: String): Map[Int, Double] = {
+    data.rdd.map(x => (x.getInt(x.fieldIndex(labelCol)), x.getDouble(x.fieldIndex(column)))).collectAsMap()
+  }
+
+  def fillDoubleValue(target: Int, mapTargetValue: Map[Int, Double]): Double = {
+    mapTargetValue(target)
+  }
+
+  def fillIntValue(target: Int, mapTargetValue: Map[Int, Double]): Double = {
+    mapTargetValue(target).round.toInt
   }
 
 }
