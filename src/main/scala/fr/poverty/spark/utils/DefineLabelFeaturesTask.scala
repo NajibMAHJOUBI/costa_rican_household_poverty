0ca118a0e9740a2cced56052397ff6dd68130abc
ubuntu <najib.mahjoubi@gmail.com>
2018-07-24 22:16:33 +0200

    add methods defineLabelFeatures, defineLabelValues and setFeatureNames

--- src/main/scala/fr/poverty/spark/utils/DefineLabelFeaturesTask.scala
+++ src/main/scala/fr/poverty/spark/utils/DefineLabelFeaturesTask.scala
@@ -1,10 +1,9 @@
 package fr.poverty.spark.utils
 
-import org.apache.spark.ml.linalg.{Vector, Vectors}
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.{col, udf}
 
 import scala.io.Source
-import scala.math.log
 
 class DefineLabelFeaturesTask(val labelColumn: String) {
 
@@ -12,14 +11,29 @@ class DefineLabelFeaturesTask(val labelColumn: String) {
 
   def run(data: DataFrame): Unit = {
     featureNames = readFeatureNames()
+  }
 
+  def setFeatureNames(features: Array[String]): Unit = {
+    featureNames = features
   }
 
   def readFeatureNames(): Array[String] = {
     Source.fromFile("src/main/resources/featuresNames").getLines.toList(0).split(",")
   }
 
-  def featuresVector() = {}
+  def defineLabelFeatures(spark: SparkSession, data: DataFrame): DataFrame = {
+    val labelValues = defineLabelValues(spark, data)
+    val getDenseVector = udf((values: List[Double]) => UtilsObject.defineDenseVector(values))
+    labelValues.withColumn("features", getDenseVector(col("values"))).drop("values")
+  }
 
+  def defineLabelValues(spark: SparkSession, data: DataFrame): DataFrame = {
+    val featuresBroadcast = spark.sparkContext.broadcast(featureNames)
+    val labelBroadcast = spark.sparkContext.broadcast(labelColumn)
+    val rdd = data.rdd
+      .map(p => (p.getInt(p.fieldIndex(labelBroadcast.value)),
+        featuresBroadcast.value.map(feature => p.getDouble(p.fieldIndex(feature))).toList))
+    spark.createDataFrame(rdd).toDF(labelColumn, "values")
+  }
 
 }
