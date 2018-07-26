c386900ab89ce9f19282bd59c28fc803c231169c
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-26 18:06:33 +0200

    add distinct between none features and  yes/no features + deal with train and test data set (target feature)

--- src/main/scala/fr/poverty/spark/utils/DefineLabelFeaturesTask.scala
+++ src/main/scala/fr/poverty/spark/utils/DefineLabelFeaturesTask.scala
@@ -1,13 +1,13 @@
 package fr.poverty.spark.utils
 
-import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions.{col, udf}
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
-import scala.io.Source
 import scala.collection.mutable.WrappedArray
+import scala.io.Source
 
 
-class DefineLabelFeaturesTask(val labelColumn: String, val sourcePath: String) {
+class DefineLabelFeaturesTask(val idColumn: String, val labelColumn: String, val sourcePath: String) {
 
   private var featureNames: Array[String] = _
 
@@ -25,7 +25,9 @@ class DefineLabelFeaturesTask(val labelColumn: String, val sourcePath: String) {
   }
 
   def readFeatureNames(path: String): Array[String] = {
-    Source.fromFile(path).getLines.toList(0).split(",")
+    val nullFeatures = Source.fromFile(s"$path/nullFeaturesNames").getLines.toList(0).split(",")
+    val yesFeatures = Source.fromFile(s"$path/yesNoFeaturesNames").getLines.toList(0).split(",")
+    nullFeatures ++ yesFeatures
   }
 
   def defineLabelFeatures(spark: SparkSession, data: DataFrame): DataFrame = {
@@ -36,11 +38,15 @@ class DefineLabelFeaturesTask(val labelColumn: String, val sourcePath: String) {
 
   def defineLabelValues(spark: SparkSession, data: DataFrame): DataFrame = {
     val featuresBroadcast = spark.sparkContext.broadcast(featureNames)
-    val labelBroadcast = spark.sparkContext.broadcast(labelColumn)
-    val rdd = data.rdd
-      .map(p => (p.getInt(p.fieldIndex(labelBroadcast.value)),
-        featuresBroadcast.value.map(feature => p.getDouble(p.fieldIndex(feature))).toList))
-    spark.createDataFrame(rdd).toDF(labelColumn, "values")
+    val idBroadcast = spark.sparkContext.broadcast(idColumn)
+    if (labelColumn == "") {
+      val rdd = data.rdd.map(p => (p.getString(p.fieldIndex(idBroadcast.value)), featuresBroadcast.value.map(feature => p.getDouble(p.fieldIndex(feature))).toList))
+      spark.createDataFrame(rdd).toDF(idColumn, "values")
+    } else {
+      val labelBroadcast = spark.sparkContext.broadcast(labelColumn)
+      val rdd = data.rdd.map(p => (p.getString(p.fieldIndex(idBroadcast.value)), p.getInt(p.fieldIndex(labelBroadcast.value)), featuresBroadcast.value.map(feature => p.getDouble(p.fieldIndex(feature))).toList))
+      spark.createDataFrame(rdd).toDF(idColumn, labelColumn, "values")
+    }
   }
 
 }
