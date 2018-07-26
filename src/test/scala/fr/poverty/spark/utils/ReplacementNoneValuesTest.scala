5042c6bbe8baae1118750d3a60331c3ac47bca31
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-26 18:10:10 +0200

    finalize

--- src/test/scala/fr/poverty/spark/utils/ReplacementNoneValuesTest.scala
+++ src/test/scala/fr/poverty/spark/utils/ReplacementNoneValuesTest.scala
@@ -2,6 +2,7 @@ package fr.poverty.spark.utils
 
 import org.apache.log4j.{Level, LogManager}
 import org.apache.spark.sql.functions.{col, udf}
+import org.apache.spark.sql.types.DoubleType
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.junit.{After, Before, Test}
 
@@ -25,7 +26,7 @@ class ReplacementNoneValuesTest {
   }
 
   @Test def testComputeMeanByColumns(): Unit = {
-    val replacement = new ReplacementNoneValuesTask("target", columns)
+    val replacement = new ReplacementNoneValuesTask("target", columns, Array(""))
     val meanData = replacement.computeMeanByColumns(data)
 
     assert(meanData.isInstanceOf[DataFrame])
@@ -43,18 +44,36 @@ class ReplacementNoneValuesTest {
     assert(meanB(meanB.fieldIndex("target")) == 200)
     assert(meanB(meanB.fieldIndex("x")) == 1.0)
     assert(meanB(meanB.fieldIndex("y")) == 1.0)
+
+    data.show()
+    meanData.show()
     }
 
-  @Test def testReplaceMissingValues(): Unit = {
-    val replacement = new ReplacementNoneValuesTask("target", columns)
+  @Test def testReplaceMissingNullValues(): Unit = {
+    val replacement = new ReplacementNoneValuesTask("target", columns, Array(""))
     val dataFilled = replacement.run(spark, data)
     val dataColumns = dataFilled.columns
     assert(dataColumns.length == data.columns.length)
     assert(dataColumns.contains("target"))
     columns.foreach(column => dataColumns.contains(column))
     assert(dataFilled.na.drop(columns).count() == dataFilled.count())
+    columns.foreach(column => dataFilled.schema.fields(dataFilled.schema.fieldIndex(column)).dataType == DoubleType)
   }
 
+  @Test def testReplaceMissingYesNoValues(): Unit = {
+    val dataYesNo = new LoadDataSetTask(sourcePath = "src/test/resources", format = "csv").run(spark, "replacementYesNoValues")
+    val replacement = new ReplacementNoneValuesTask("target", Array(""), columns)
+    val dataFilled = replacement.run(spark, dataYesNo)
+    val dataColumns = dataFilled.columns
+    assert(dataColumns.length == data.columns.length)
+    assert(dataColumns.contains("target"))
+    columns.foreach(column => dataColumns.contains(column))
+    assert(dataFilled.na.drop(columns).count() == dataFilled.count())
+    columns.foreach(column => dataFilled.schema.fields(dataFilled.schema.fieldIndex(column)).dataType == DoubleType)
+
+    dataYesNo.show()
+    dataFilled.show()
+  }
 
   @After def afterAll() {
     spark.stop()
