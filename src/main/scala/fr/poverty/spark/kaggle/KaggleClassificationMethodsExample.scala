36765de0c9c47fe5e0346ed6bc6f6715df3b1f73
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-27 17:55:59 +0200

    decision tree + random forest + logistic regression

--- src/main/scala/fr/poverty/spark/kaggle/KaggleClassificationMethodsExample.scala
+++ src/main/scala/fr/poverty/spark/kaggle/KaggleClassificationMethodsExample.scala
@@ -1,6 +1,6 @@
 package fr.poverty.spark.kaggle
 
-import fr.poverty.spark.classification.trainValidation.TrainValidationDecisionTreeTask
+import fr.poverty.spark.classification.trainValidation.{TrainValidationDecisionTreeTask, TrainValidationLogisticRegressionTask, TrainValidationRandomForestTask}
 import fr.poverty.spark.utils.{DefineLabelFeaturesTask, LoadDataSetTask, ReplacementNoneValuesTask}
 import org.apache.log4j.{Level, LogManager}
 import org.apache.spark.sql.SparkSession
@@ -24,25 +24,36 @@ object KaggleClassificationMethodsExample {
     val train = new LoadDataSetTask(sourcePath = "data", format = "csv").run(spark, "train")
     val test = new LoadDataSetTask(sourcePath = "data", format = "csv").run(spark, "test")
 
-
     val replacementNoneValues = new ReplacementNoneValuesTask("target", nullFeatures, yesNoFeatures).run(spark, train, test)
     val trainFilled = replacementNoneValues.getTrain
     val testFilled = replacementNoneValues.getTest
-    testFilled.show(5)
 
     val labelFeatures = new DefineLabelFeaturesTask("Id", "Target", "src/main/resources").run(spark, trainFilled)
     val labelFeaturesSubmission = new DefineLabelFeaturesTask("Id", "", "src/main/resources").run(spark, testFilled)
 
-    // --> Model
-    val decisionTree = new TrainValidationDecisionTreeTask("Target", "features", "prediction", 0.75, "target/decisionTree")
-    decisionTree.run(labelFeatures)
-    decisionTree.transform(labelFeatures)
-    decisionTree.savePrediction()
+    val model = "decisionTree"
 
-    // Submission
-    labelFeaturesSubmission.show(5) //    decisionTree.transform(labelFeaturesSubmission)
-    //    decisionTree.saveSubmission()
-
-
+    if (model == "decisionTree") {
+      val decisionTree = new TrainValidationDecisionTreeTask("Target", "features", "prediction", 0.75, "target/submission/trainValidation/decisionTree")
+      decisionTree.run(labelFeatures)
+      decisionTree.transform(labelFeatures)
+      decisionTree.savePrediction() // Submission
+      decisionTree.transform(labelFeaturesSubmission)
+      decisionTree.saveSubmission()
+    } else if (model == "randomForest") {
+      val randomForest = new TrainValidationRandomForestTask("Target", "features", "prediction", 0.75, "target/submission/trainValidation/randomForest")
+      randomForest.run(labelFeatures)
+      randomForest.transform(labelFeatures)
+      randomForest.savePrediction() // Submission
+      randomForest.transform(labelFeaturesSubmission)
+      randomForest.saveSubmission()
+    } else if (model == "logisticRegression") {
+      val logisticRegression = new TrainValidationLogisticRegressionTask("Target", "features", "prediction", 0.75, "target/submission/trainValidation/logisticRegression")
+      logisticRegression.run(labelFeatures)
+      logisticRegression.transform(labelFeatures)
+      logisticRegression.savePrediction() // Submission
+      logisticRegression.transform(labelFeaturesSubmission)
+      logisticRegression.saveSubmission()
+    }
   }
 }
