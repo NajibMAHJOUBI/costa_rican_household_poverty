b1941e4c10df6aa7ba453595691b8de405632736
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-07-30 18:10:04 +0200

    first commit

---
+++ src/main/scala/fr/poverty/spark/classification/crossValidation/CrossValidationOneVsRestTask.scala
@@ -0,0 +1,57 @@
+package fr.poverty.spark.classification.crossValidation
+
+import fr.poverty.spark.classification.gridParameters.GridParametersOneVsRest
+import fr.poverty.spark.classification.task.{CrossValidationModelFactory, OneVsRestTask}
+import org.apache.spark.ml.classification.{OneVsRest, OneVsRestModel}
+import org.apache.spark.ml.tuning.CrossValidator
+import org.apache.spark.sql.DataFrame
+
+
+class CrossValidationOneVsRestTask(override val labelColumn: String,
+                                   override val featureColumn: String,
+                                   override val predictionColumn: String,
+                                   override val numFolds: Integer,
+                                   override val pathSave: String,
+                                   val classifier: String)
+  extends CrossValidationTask(labelColumn, featureColumn, predictionColumn, numFolds, pathSave)
+    with CrossValidationModelFactory {
+
+  var estimator: OneVsRest = _
+
+  override def run(data: DataFrame): CrossValidationOneVsRestTask = {
+    defineEstimator()
+    defineGridParameters()
+    defineEvaluator()
+    defineCrossValidatorModel()
+    fit(data)
+    saveModel()
+    this
+  }
+
+  override def defineEstimator(): CrossValidationOneVsRestTask = {
+    estimator = new OneVsRestTask(labelColumn, featureColumn, predictionColumn, classifier).defineModel.getModel
+    this
+  }
+
+  override def defineGridParameters(): CrossValidationOneVsRestTask = {
+    paramGrid = GridParametersOneVsRest.getParamsGrid(estimator, classifier, labelColumn, featureColumn, predictionColumn)
+    this
+  }
+
+  override def defineCrossValidatorModel(): CrossValidationOneVsRestTask = {
+    crossValidator = new CrossValidator()
+      .setEvaluator(evaluator)
+      .setEstimatorParamMaps(paramGrid)
+      .setEstimator(estimator)
+      .setNumFolds(numFolds)
+    this
+  }
+
+  def getEstimator: OneVsRest = {
+    estimator
+  }
+
+  def getBestModel: OneVsRestModel = {
+    crossValidatorModel.bestModel.asInstanceOf[OneVsRestModel]
+  }
+}
---
+++ src/test/scala/fr/poverty/spark/classification/crossValidation/CrossValidationOneVsRestTaskTest.scala
@@ -0,0 +1,74 @@
+package fr.poverty.spark.classification.crossValidation
+
+import fr.poverty.spark.utils.LoadDataSetTask
+import org.apache.log4j.{Level, LogManager}
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation.Evaluator
+import org.apache.spark.ml.param.ParamMap
+import org.apache.spark.ml.tuning.CrossValidator
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.junit.{After, Before, Test}
+import org.scalatest.junit.AssertionsForJUnit
+
+/**
+  * Created by mahjoubi on 12/06/18.
+  *
+  * Cross-validation of decision tree classifier model
+  *
+  */
+
+
+class CrossValidationOneVsRestTaskTest extends AssertionsForJUnit {
+
+  private val labelColumn: String = "target"
+  private val featureColumn: String = "features"
+  private val predictionColumn: String = "prediction"
+  private var spark: SparkSession = _
+
+  @Before def beforeAll() {
+    spark = SparkSession
+      .builder
+      .master("local")
+      .appName("test cross validator one vs rest")
+      .getOrCreate()
+    val log = LogManager.getRootLogger
+    log.setLevel(Level.WARN)
+  }
+
+  @Test def testCrossValidationDecisionTree(): Unit = {
+    val data = new LoadDataSetTask("src/test/resources", "parquet").run(spark, "classificationTask")
+
+    val cv = new CrossValidationOneVsRestTask(
+      labelColumn = labelColumn,
+      featureColumn = featureColumn,
+      predictionColumn = predictionColumn,
+      numFolds = 3,
+      pathSave = "target/validation/crossValidation/decisionTree",
+      classifier="decisionTree")
+    cv.run(data)
+
+    assert(cv.getLabelColumn == labelColumn)
+    assert(cv.getFeatureColumn == featureColumn)
+    assert(cv.getPredictionColumn == predictionColumn)
+    assert(cv.getGridParameters.isInstanceOf[Array[ParamMap]])
+    assert(cv.getEstimator.isInstanceOf[DecisionTreeClassifier])
+    assert(cv.getEvaluator.isInstanceOf[Evaluator])
+    assert(cv.getCrossValidator.isInstanceOf[CrossValidator])
+
+    val bestModel = cv.getBestModel
+    assert(bestModel.getLabelCol == labelColumn)
+    assert(bestModel.getFeaturesCol == featureColumn)
+    assert(bestModel.getPredictionCol == predictionColumn)
+
+    cv.transform(data)
+    val transform = cv.getPrediction
+    assert(transform.isInstanceOf[DataFrame])
+    assert(transform.count() == data.count())
+    assert(transform.columns.contains(predictionColumn))
+    }
+
+  @After def afterAll() {
+    spark.stop()
+  }
+
+}
