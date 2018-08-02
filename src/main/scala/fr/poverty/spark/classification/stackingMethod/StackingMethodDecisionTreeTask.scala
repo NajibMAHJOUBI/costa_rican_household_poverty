bcd37c4003429f0a2f2848d36e170eb607827704
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-08-02 16:53:03 +0200

    first commit

--- src/main/scala/fr/poverty/spark/classification/stackingMethod/StackingMethodDecisionTreeTask.scala
+++ src/main/scala/fr/poverty/spark/classification/stackingMethod/StackingMethodDecisionTreeTask.scala
@@ -4,18 +4,16 @@ import org.apache.spark.ml.classification.DecisionTreeClassificationModel
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 
-class StackingMethodDecisionTreeTask(override val classificationMethods: Array[String],
-                                     override val pathTrain: String,
-                                     override val pathPrediction: String,
+class StackingMethodDecisionTreeTask(override val classificationMethod: String,
+                                     override val pathPrediction: List[String], override val formatPrediction: String,
+                                     override val pathTrain: String, override val formatTrain: String,
                                      override val pathSave: String,
                                      override val validationMethod: String,
                                      override val ratio: Double,
-                                     override val idColumn: String,
+                                     override  val idColumn: String,
                                      override val labelColumn: String,
-                                     override val predictionColumn: String,
-                                     val numFolds: Int,
-                                     val trainRation: Double)
-  extends StackingMethodTask(classificationMethods, pathTrain, pathPrediction, pathSave, validationMethod, ratio, idColumn,
+                                     override val predictionColumn: String)
+  extends StackingMethodTask(classificationMethod, pathPrediction, formatPrediction, pathTrain, formatTrain, pathSave, validationMethod, ratio, idColumn,
     labelColumn, predictionColumn)
     with StackingMethodFactory {
 
