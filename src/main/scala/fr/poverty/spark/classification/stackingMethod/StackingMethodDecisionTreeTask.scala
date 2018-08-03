5264e8d0ef75336884049cd59969a2464f914d2b
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-08-03 17:48:27 +0200

    add defineModel method

--- src/main/scala/fr/poverty/spark/classification/stackingMethod/StackingMethodDecisionTreeTask.scala
+++ src/main/scala/fr/poverty/spark/classification/stackingMethod/StackingMethodDecisionTreeTask.scala
@@ -1,12 +1,12 @@
 package fr.poverty.spark.classification.stackingMethod
 
-import fr.poverty.spark.classification.task.DecisionTreeTask
-import org.apache.spark.ml.classification.{DecisionTreeClassificationModel, DecisionTreeClassifier}
+import fr.poverty.spark.classification.crossValidation.CrossValidationDecisionTreeTask
+import fr.poverty.spark.classification.trainValidation.TrainValidationDecisionTreeTask
+import org.apache.spark.ml.classification.DecisionTreeClassificationModel
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 
-class StackingMethodDecisionTreeTask(override val classificationMethod: String,
-                                     override val pathPrediction: List[String], override val formatPrediction: String,
+class StackingMethodDecisionTreeTask(override val pathPrediction: List[String], override val formatPrediction: String,
                                      override val pathTrain: String, override val formatTrain: String,
                                      override val pathSave: String,
                                      override val validationMethod: String,
@@ -14,37 +14,38 @@ class StackingMethodDecisionTreeTask(override val classificationMethod: String,
                                      override  val idColumn: String,
                                      override val labelColumn: String,
                                      override val predictionColumn: String)
-  extends StackingMethodTask(classificationMethod, pathPrediction, formatPrediction, pathTrain, formatTrain, pathSave, validationMethod, ratio, idColumn,
+  extends StackingMethodTask(pathPrediction, formatPrediction, pathTrain, formatTrain, pathSave, validationMethod, ratio, idColumn,
     labelColumn, predictionColumn)
     with StackingMethodFactory {
 
   val featureColumn: String = "features"
-  var model: DecisionTreeClassifier = _
-  var modelFit: DecisionTreeClassificationModel = _
+  var model: DecisionTreeClassificationModel = _
 
   override def run(spark: SparkSession): StackingMethodDecisionTreeTask = {
-    val labelFeatures = new StackingMethodTask(classificationMethod, pathPrediction, formatPrediction, pathTrain,
+    labelFeatures = new StackingMethodTask(pathPrediction, formatPrediction, pathTrain,
       formatTrain, pathSave, validationMethod, ratio, idColumn, labelColumn, predictionColumn).createLabelFeatures(spark)
-    defineModel()
-    fit(labelFeatures)
+    defineValidationModel(labelFeatures)
     this
   }
 
-  override def defineModel(): StackingMethodDecisionTreeTask = {
-    model = new DecisionTreeTask(labelColumn=labelColumn,
-                                     featureColumn=featureColumn,
-                                     predictionColumn=predictionColumn).defineModel.getModel
+  override def defineValidationModel(data: DataFrame): StackingMethodDecisionTreeTask = {
+    if (validationMethod == "crossValidation") {
+      val cv = new CrossValidationDecisionTreeTask(labelColumn = labelColumn,
+        featureColumn = featureColumn, predictionColumn = "prediction", numFolds = ratio.toInt,
+        pathSave = "")
+      cv.run(data)
+      model = cv.getBestModel
+    } else if (validationMethod == "trainValidation") {
+      val tv = new TrainValidationDecisionTreeTask(labelColumn, featureColumn,
+        "prediction", trainRatio=ratio.toDouble, "")
+      tv.run(data)
+      model = tv.getBestModel
+    }
     this
   }
 
-  override def fit(data: DataFrame): StackingMethodDecisionTreeTask = {
-    modelFit = model.fit(data)
-    this
-  }
-
-  override def transform(data: DataFrame): StackingMethodDecisionTreeTask = {
-    prediction = modelFit.transform(data)
-    this
+  override def transform(data: DataFrame): DataFrame = {
+    model.transform(data)
   }
 
   override def saveModel(path: String): StackingMethodDecisionTreeTask = {
@@ -53,7 +54,7 @@ class StackingMethodDecisionTreeTask(override val classificationMethod: String,
   }
 
   def loadModel(path: String): StackingMethodDecisionTreeTask = {
-    modelFit = DecisionTreeClassificationModel.load(path)
+    model = DecisionTreeClassificationModel.load(path)
     this
   }
 }
