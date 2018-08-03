96dd54b6ed3265c663363375470ca10e8354c376
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-08-03 17:48:15 +0200

    add defineModel method

--- src/main/scala/fr/poverty/spark/classification/stackingMethod/StackingMethodLogisticRegressionTask.scala
+++ src/main/scala/fr/poverty/spark/classification/stackingMethod/StackingMethodLogisticRegressionTask.scala
@@ -1,10 +1,11 @@
 package fr.poverty.spark.classification.stackingMethod
 
+import fr.poverty.spark.classification.crossValidation.CrossValidationLogisticRegressionTask
+import fr.poverty.spark.classification.trainValidation.TrainValidationLogisticRegressionTask
 import org.apache.spark.ml.classification.LogisticRegressionModel
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-class StackingMethodLogisticRegressionTask(override val classificationMethod: String,
-                                           override val pathPrediction: List[String], override val formatPrediction: String,
+class StackingMethodLogisticRegressionTask(override val pathPrediction: List[String], override val formatPrediction: String,
                                            override val pathTrain: String, override val formatTrain: String,
                                            override val pathSave: String,
                                            override val validationMethod: String,
@@ -12,7 +13,7 @@ class StackingMethodLogisticRegressionTask(override val classificationMethod: St
                                            override  val idColumn: String,
                                            override val labelColumn: String,
                                            override val predictionColumn: String)
-  extends StackingMethodTask(classificationMethod, pathPrediction, formatPrediction, pathTrain, formatTrain, pathSave, validationMethod, ratio, idColumn,
+  extends StackingMethodTask(pathPrediction, formatPrediction, pathTrain, formatTrain, pathSave, validationMethod, ratio, idColumn,
     labelColumn, predictionColumn)
     with StackingMethodFactory {
 
@@ -20,22 +21,40 @@ class StackingMethodLogisticRegressionTask(override val classificationMethod: St
   var model: LogisticRegressionModel = _
 
   override def run(spark: SparkSession): StackingMethodLogisticRegressionTask = {
+    labelFeatures = new StackingMethodTask(pathPrediction, formatPrediction, pathTrain,
+      formatTrain, pathSave, validationMethod, ratio, idColumn, labelColumn, predictionColumn).createLabelFeatures(spark)
+    defineValidationModel(labelFeatures)
     this
   }
 
-  override def computeModel(data: DataFrame, label: String): StackingMethodLogisticRegressionTask = {
+  override def defineValidationModel(data: DataFrame): StackingMethodLogisticRegressionTask = {
+    if (validationMethod == "crossValidation") {
+      val cv = new CrossValidationLogisticRegressionTask(labelColumn,
+        featureColumn, "prediction", numFolds = ratio.toInt,
+        pathSave = "")
+      cv.run(data)
+      model = cv.getBestModel
+    } else if (validationMethod == "trainValidation") {
+      val tv = new TrainValidationLogisticRegressionTask(labelColumn, featureColumn,
+        "prediction", trainRatio=ratio.toDouble, "")
+      tv.run(data)
+      model = tv.getBestModel
+    }
     this
   }
 
-  override def saveModel(column: String): StackingMethodLogisticRegressionTask = {
-    this
+
+  override def transform(data: DataFrame): DataFrame = {
+    model.transform(data)
   }
 
-  override def computePrediction(data: DataFrame): StackingMethodLogisticRegressionTask = {
+  override def saveModel(path: String): StackingMethodLogisticRegressionTask = {
+    model.write.overwrite().save(path)
     this
   }
 
   def loadModel(path: String): StackingMethodLogisticRegressionTask = {
+    model = LogisticRegressionModel.load(path)
     this
   }
 }
