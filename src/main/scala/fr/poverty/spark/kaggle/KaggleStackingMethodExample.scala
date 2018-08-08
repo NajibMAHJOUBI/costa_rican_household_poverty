b97cde13e18e512955c5249fa72f3fc21617dbf0
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-08-08 16:40:39 +0200

    continue implementation

--- src/main/scala/fr/poverty/spark/kaggle/KaggleStackingMethodExample.scala
+++ src/main/scala/fr/poverty/spark/kaggle/KaggleStackingMethodExample.scala
@@ -1,5 +1,6 @@
 package fr.poverty.spark.kaggle
 
+import fr.poverty.spark.classification.stackingMethod.StackingMethodDecisionTreeTask
 import org.apache.log4j.{Level, LogManager}
 import org.apache.spark.sql.SparkSession
 
@@ -17,6 +18,27 @@ object KaggleStackingMethodExample {
     val labelColumn = "label"
     val featureColumn = "features"
     val predictionColumn = "prediction"
+    val pathTrain = "data"
+
+    val methodValidation = "trainValidation/trainRatio" // "crossValidation/numFolds_"
+    val ratio = 50
+    val pathPrediction = "submission/"
+    var listPathPrediction: List[String] = List("decisionTree", "logisticRegression", "randomForest", "naiveBayes").map(method => s"$pathPrediction/${methodValidation}_$ratio/$method")
+    listPathPrediction = listPathPrediction ++  List("decisionTree", "logisticRegression", "randomForest", "naiveBayes").map(method => s"$pathPrediction/${methodValidation}_$ratio/oneVsRest/$method")
+
+
+    val stackingMethodDecisionTree = new StackingMethodDecisionTreeTask(idColumn, labelColumn, predictionColumn,
+      listPathPrediction, "parquet", pathTrain, "csv",
+      s"$pathPrediction/${methodValidation}_$ratio/stringIndexer",
+      "submission/stackingMethod", "trainValidation", ratio/100.0)
+
+
+//    override val idColumn: String, override val labelColumn: String, override val predictionColumn: String,
+//    override val pathPrediction: List[String], override val formatPrediction: String,
+//    override val pathTrain: String, override val formatTrain: String,
+//    override val pathStringIndexer: String, override val pathSave: String,
+//    override val validationMethod: String, override val ratio: Double
+
 
 
   }
