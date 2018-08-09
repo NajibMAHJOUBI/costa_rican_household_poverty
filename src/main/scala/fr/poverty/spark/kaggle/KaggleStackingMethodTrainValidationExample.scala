70763852f7624278a7ba5d30379b7ad1d5f48ec0
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-08-09 16:35:51 +0200

    renamed

---
+++ src/main/scala/fr/poverty/spark/kaggle/KaggleStackingMethodTrainValidationExample.scala
@@ -0,0 +1,78 @@
+package fr.poverty.spark.kaggle
+
+import fr.poverty.spark.classification.stackingMethod._
+import org.apache.log4j.{Level, LogManager}
+import org.apache.spark.sql.SparkSession
+
+object KaggleStackingMethodTrainValidationExample {
+
+  def main(arguments: Array[String]): Unit = {
+    val spark = SparkSession.builder.master("local").appName("Kaggle Submission Example - Stacking Method").getOrCreate()
+
+    val log = LogManager.getRootLogger
+    log.setLevel(Level.WARN)
+
+    // --> Initialization
+    val idColumn = "Id"
+    val labelColumn = "Target"
+    val predictionColumn = "prediction"
+    val pathTrain = "data"
+    val mapFormat = Map("prediction" -> "parquet", "submission" -> "csv")
+
+    val methodValidation = "trainValidation" // "crossValidation/numFolds_"
+    val pathPrediction = "submission/"
+    val models = List("decisionTree", "randomForest", "logisticRegression", "naiveBayes")
+
+    Array(50, 60, 70, 75).foreach(ratio => {
+      println(s"Ratio: $ratio")
+      var listPathPrediction: List[String] = models.map(method => s"$pathPrediction/${methodValidation}/trainRatio_$ratio/$method")
+      listPathPrediction = listPathPrediction ++  models.map(method => s"$pathPrediction/${methodValidation}/trainRatio_$ratio/oneVsRest/$method")
+      (models ++ List("oneVsRest")).foreach(model =>{
+        println(s"Model: $model")
+        if (model == "decisionTree") {
+          val stackingMethodDecisionTree = new StackingMethodDecisionTreeTask(idColumn, labelColumn, predictionColumn,
+            listPathPrediction, mapFormat, pathTrain, "csv",
+            s"$pathPrediction/$methodValidation/modelStringIndexer",
+            s"submission/stackingMethod/$methodValidation/trainRatio_$ratio/$model", "trainValidation", ratio/100.0)
+          stackingMethodDecisionTree.run(spark)
+        } else if(model == "randomForest"){
+          val stackingMethodRandomForest = new StackingMethodRandomForestTask(idColumn, labelColumn, predictionColumn,
+            listPathPrediction, mapFormat, pathTrain, "csv",
+            s"$pathPrediction/$methodValidation/modelStringIndexer",
+            s"submission/stackingMethod/$methodValidation/trainRatio_$ratio/$model", "trainValidation", ratio/100.0)
+          stackingMethodRandomForest.run(spark)
+        } else if (model == "logisticRegression") {
+          val stackingMethodLogisticRegression = new StackingMethodLogisticRegressionTask(idColumn, labelColumn, predictionColumn,
+          listPathPrediction, mapFormat, pathTrain, "csv",
+          s"$pathPrediction/$methodValidation/modelStringIndexer",
+          s"submission/stackingMethod/$methodValidation/trainRatio_$ratio/$model", "trainValidation", ratio/100.0)
+          stackingMethodLogisticRegression.run(spark)
+        } else if (model == "oneVsRest"){
+          models.foreach(classifier => {
+            println(s"Classifier: $classifier")
+            val stackingMethodOneVsRest = new StackingMethodOneVsRestTask(idColumn, labelColumn, predictionColumn,
+              listPathPrediction, mapFormat, pathTrain, "csv",
+              s"$pathPrediction/$methodValidation/modelStringIndexer",
+              s"submission/stackingMethod/$methodValidation/trainRatio_$ratio/$model/$classifier", "trainValidation", ratio/100.0, classifier=classifier, false)
+            stackingMethodOneVsRest.run(spark)
+          })
+        } else if (model == "naiveBayes"){
+          val stackingMethodNaiveBayes = new StackingMethodNaiveBayesTask(idColumn, labelColumn, predictionColumn,
+            listPathPrediction, mapFormat, pathTrain, "csv",
+            s"$pathPrediction/$methodValidation/modelStringIndexer",
+            s"submission/stackingMethod/$methodValidation/trainRatio_$ratio/$model", "trainValidation", ratio/100.0, false)
+          stackingMethodNaiveBayes.run(spark)
+        }
+
+
+      })
+
+
+    })
+
+
+
+
+
+  }
+}
