0c1a610c1632f8cc09c5426c1ece3cda7eba65d5
ubuntu <najib.mahjoubi@gmail.com>
2018-08-16 21:43:18 +0200

    remove methods to Bagging Task

--- src/main/scala/fr/poverty/spark/classification/bagging/BaggingLogisticRegressionTask.scala
+++ src/main/scala/fr/poverty/spark/classification/bagging/BaggingLogisticRegressionTask.scala
@@ -3,8 +3,7 @@ package fr.poverty.spark.classification.bagging
 import fr.poverty.spark.classification.validation.crossValidation.CrossValidationLogisticRegressionTask
 import fr.poverty.spark.classification.validation.trainValidation.TrainValidationLogisticRegressionTask
 import org.apache.spark.ml.classification.LogisticRegressionModel
-import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.DataFrame
 
 class BaggingLogisticRegressionTask(override val idColumn: String, override val labelColumn: String,
                                     override val featureColumn: String, override val predictionColumn: String,
@@ -35,22 +34,5 @@ class BaggingLogisticRegressionTask(override val idColumn: String, override val 
     })
   }
 
-  def computePrediction(spark: SparkSession, data: DataFrame): DataFrame = {
-    val idColumnBroadcast = spark.sparkContext.broadcast(idColumn)
-    val labelColumnBroadcast = spark.sparkContext.broadcast(labelColumn)
-    val numberOfModels = spark.sparkContext.broadcast(modelFittedList.length)
-    var idDataSet: DataFrame = data.select(idColumn, labelColumn)
-    modelFittedList.foreach(model => {
-      val weakTransform = model.transform(data).select(idColumn, predictionColumn)
-        .select(col(idColumn), col("prediction").alias(s"prediction_${modelFittedList.indexOf(model)}"))
-      idDataSet = idDataSet.join(weakTransform, Seq(idColumn))
-    })
-    val rdd = idDataSet.rdd.map(p => (p.getString(p.fieldIndex(idColumnBroadcast.value)),
-      p.getDouble(p.fieldIndex(labelColumnBroadcast.value)),
-      mergePredictions(p, numberOfModels.value)))
-    spark.createDataFrame(rdd).toDF(idColumn, labelColumn, predictionColumn)
-  }
-
-  def computeSubmission(data: DataFrame): Unit = {}
-
+  def getModels: List[LogisticRegressionModel] = modelFittedList
 }
