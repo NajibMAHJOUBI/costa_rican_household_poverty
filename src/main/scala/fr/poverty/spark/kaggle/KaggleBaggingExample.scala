ec2736871cf6889f3fc00a4a865f94fbcd51f133
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-08-17 16:42:26 +0200

    complete with all calassification methods

--- src/main/scala/fr/poverty/spark/kaggle/KaggleBaggingExample.scala
+++ src/main/scala/fr/poverty/spark/kaggle/KaggleBaggingExample.scala
@@ -1,6 +1,6 @@
 package fr.poverty.spark.kaggle
 
-import fr.poverty.spark.classification.bagging.{BaggingLogisticRegressionTask}
+import fr.poverty.spark.classification.bagging._
 import fr.poverty.spark.utils._
 import org.apache.log4j.{Level, LogManager}
 import org.apache.spark.sql.SparkSession
@@ -24,9 +24,9 @@ object KaggleBaggingExample {
     val weightColumn = "weight"
     val trainRatioList = List(50, 60, 70, 75)
     val sourcePath = "src/main/resources"
-    val savePath = "submission/bagging"
-    val models = List("logisticRegression")
+    val models = List("logisticRegression", "decisionTree", "randomForest", "naiveBayes")
     val validationMethod: String = "trainValidation"
+    val savePath = s"submission/bagging/$validationMethod"
     val ratio: Double = 0.5
 
     // --> features name
@@ -51,9 +51,9 @@ object KaggleBaggingExample {
 
     trainRatioList.foreach(ratio => {
       println(s"ratio: $ratio")
-      models.foreach(model => {
+      (models ++ List("oneVsRest")).foreach(model => {
+        println(s"Model: $model")
         if(model == "logisticRegression"){
-          println(s"Model: $model")
           val logisticRegression = new BaggingLogisticRegressionTask(idColumn, labelColumn, featureColumn,
             predictionColumn, s"$savePath/trainRatio${ratio.toString}/$model",
             5, 0.75, validationMethod, ratio.toDouble/100.0)
@@ -62,6 +62,49 @@ object KaggleBaggingExample {
           val submission = logisticRegression.computeSubmission(spark, labelFeaturesSubmission, logisticRegression.getModels)
           logisticRegression.savePrediction(indexToString.run(prediction))
           logisticRegression.saveSubmission(indexToString.run(submission))
+        }
+        else if(model == "randomForest"){
+          val randomForest = new BaggingRandomForestTask(idColumn, labelColumn, featureColumn,
+            predictionColumn, s"$savePath/trainRatio${ratio.toString}/$model",
+            5, 0.75, validationMethod, ratio.toDouble/100.0)
+          randomForest.run(labelFeaturesIndexed)
+          val prediction = randomForest.computePrediction(spark, labelFeaturesIndexed, randomForest.getModels)
+          val submission = randomForest.computeSubmission(spark, labelFeaturesSubmission, randomForest.getModels)
+          randomForest.savePrediction(indexToString.run(prediction))
+          randomForest.saveSubmission(indexToString.run(submission))
+        }
+        else if(model == "decisionTree"){
+          val decisionTree = new BaggingDecisionTreeTask(idColumn, labelColumn, featureColumn,
+            predictionColumn, s"$savePath/trainRatio${ratio.toString}/$model",
+            5, 0.75, validationMethod, ratio.toDouble/100.0)
+          decisionTree.run(labelFeaturesIndexed)
+          val prediction = decisionTree.computePrediction(spark, labelFeaturesIndexed, decisionTree.getModels)
+          val submission = decisionTree.computeSubmission(spark, labelFeaturesSubmission, decisionTree.getModels)
+          decisionTree.savePrediction(indexToString.run(prediction))
+          decisionTree.saveSubmission(indexToString.run(submission))
+        }
+        else if(model == "naiveBayes"){
+          val naiveBayes = new BaggingNaiveBayesTask(idColumn, labelColumn, featureColumn,
+            predictionColumn, s"$savePath/trainRatio${ratio.toString}/$model",
+            5, 0.75, validationMethod, ratio.toDouble/100.0, false)
+          naiveBayes.run(labelFeaturesIndexed)
+          val prediction = naiveBayes.computePrediction(spark, labelFeaturesIndexed, naiveBayes.getModels)
+          val submission = naiveBayes.computeSubmission(spark, labelFeaturesSubmission, naiveBayes.getModels)
+          naiveBayes.savePrediction(indexToString.run(prediction))
+          naiveBayes.saveSubmission(indexToString.run(submission))
+        }
+        else if(model == "oneVsRest"){
+          models.foreach(classifier => {
+            val oneVsRest = new BaggingOneVsRestTask(idColumn, labelColumn, featureColumn,
+              predictionColumn, s"$savePath/trainRatio${ratio.toString}/$model/$classifier",
+              5, 0.75, validationMethod, ratio.toDouble/100.0, classifier)
+            oneVsRest.run(labelFeaturesIndexed)
+            val prediction = oneVsRest.computePrediction(spark, labelFeaturesIndexed, oneVsRest.getModels)
+            val submission = oneVsRest.computeSubmission(spark, labelFeaturesSubmission, oneVsRest.getModels)
+            oneVsRest.savePrediction(indexToString.run(prediction))
+            oneVsRest.saveSubmission(indexToString.run(submission))
+          })
+
         }
       })
     })
