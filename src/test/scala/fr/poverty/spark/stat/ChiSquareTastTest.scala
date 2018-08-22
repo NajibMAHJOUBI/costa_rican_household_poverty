b33946856694eb82fae146b8ce654084220d3575
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-08-22 16:48:45 +0200

    reformat

--- src/test/scala/fr/poverty/spark/stat/ChiSquareTastTest.scala
+++ src/test/scala/fr/poverty/spark/stat/ChiSquareTastTest.scala
@@ -1,11 +1,13 @@
 package fr.poverty.spark.stat
 
-import fr.poverty.spark.sampleMinorityClass.SampleMinorityClassTask
 import fr.poverty.spark.utils.LoadDataSetTask
 import org.apache.log4j.{Level, LogManager}
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
-import org.apache.spark.sql.types.{DoubleType, IntegerType, StructField, StructType}
+import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.sql.functions.{col, udf}
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.junit.{After, Before, Test}
+
+import scala.io.Source
 
 class ChiSquareTastTest {
 
@@ -25,10 +27,23 @@ class ChiSquareTastTest {
   }
 
   @Test def testChiSquare(): Unit = {
+    val categoricalFeatures = Source
+      .fromFile("/home/mahjoubi/Documents/github/costa_rican_household_poverty/src/main/resources/categoricalFeatures")
+      .getLines
+      .toList
+      .head
+      .split(",").toList
     val data = new LoadDataSetTask(sourcePath = "data", format = "csv").run(spark, "train")
-    val featuresList = List("paredblolad", "paredzocalo", "paredpreb", "pareddes", "paredmad", "paredzinc", "paredfibras", "paredother")
-    val chiSelector = new ChiSquareTask("Target", featuresList, 0.05)
-    chiSelector.run(spark, data).show(false)
+//    val featuresList = List("paredblolad", "paredzocalo", "paredpreb", "pareddes", "paredmad", "paredzinc", "paredfibras", "paredother")
+    val chiSelector = new ChiSquareTask("Target", categoricalFeatures, 0.05)
+
+    val vectorSize = udf((x: Vector) => x.size)
+
+    val selected = chiSelector.run(spark, data)
+      .withColumn("featuresSize", vectorSize(col("features")))
+      .withColumn("selectedSize", vectorSize(col("selectedFeatures")))
+
+    selected.show(false)
   }
 
 
