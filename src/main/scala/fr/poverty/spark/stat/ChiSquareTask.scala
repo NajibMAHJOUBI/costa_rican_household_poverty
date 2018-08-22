9d98cdc94793c94b93f79c1b7eb854d57524788e
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-08-22 16:47:36 +0200

    deal train and test datasets in the same time

--- src/main/scala/fr/poverty/spark/stat/ChiSquareTask.scala
+++ src/main/scala/fr/poverty/spark/stat/ChiSquareTask.scala
@@ -4,34 +4,76 @@ import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.functions.{col, udf}
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.ml.feature.ChiSqSelector
+import org.apache.spark.ml.feature.{ChiSqSelector, ChiSqSelectorModel}
 
 import scala.collection.mutable
 
-class ChiSquareTask(val labelColumn: String, val featuresColumn: List[String], alpha: Double) {
+class ChiSquareTask(val idColumn: String, val labelColumn: String, val featuresColumn: List[String], val featureColumn: String, alpha: Double) {
 
-  def run(spark: SparkSession, data: DataFrame): DataFrame = {
-    val labelFeatures = defineLabelFeatures(spark, data)
-    ChiSquareSelector(labelFeatures)
+  private var model: ChiSqSelectorModel = _
+  private var trainLabelFeatures: DataFrame = _
+  private var testLabelFeatures: DataFrame = _
+
+  def run(spark: SparkSession, train: DataFrame, test: DataFrame): ChiSquareTask = {
+    defineLabelFeatures(spark, train, "train")
+    defineLabelFeatures(spark, test, "test")
+    fit(trainLabelFeatures)
+    this
   }
 
-  def defineLabelValues(spark: SparkSession, data: DataFrame): RDD[(Double, Array[Double])] = {
+  def defineTrainLabelValues(spark: SparkSession, data: DataFrame): RDD[(String, Double, Array[Double])] = {
+    val idBroadcast = spark.sparkContext.broadcast(idColumn)
     val labelBroadcast = spark.sparkContext.broadcast(labelColumn)
     val featuresBroadcast = spark.sparkContext.broadcast(featuresColumn)
-    data.rdd.map(row => (row.getInt(row.fieldIndex(labelBroadcast.value)).toDouble, StatObject.extractValues(row, featuresBroadcast.value)))
+    data.rdd.map(row => (row.getString(row.fieldIndex(idBroadcast.value)),
+      row.getInt(row.fieldIndex(labelBroadcast.value)),
+      StatObject.extractValues(row, featuresBroadcast.value)))
   }
 
-  def defineLabelFeatures(spark: SparkSession, data: DataFrame): DataFrame = {
+
+  def defineTestLabelValues(spark: SparkSession, data: DataFrame): RDD[(String, Array[Double])] = {
+    val idBroadcast = spark.sparkContext.broadcast(idColumn)
+    val featuresBroadcast = spark.sparkContext.broadcast(featuresColumn)
+    data.rdd.map(row => (row.getString(row.fieldIndex(idBroadcast.value)), StatObject.extractValues(row, featuresBroadcast.value)))
+
+  }
+
+  def defineLabelFeatures(spark: SparkSession, data: DataFrame, option: String): ChiSquareTask = {
     val defineFeatures = udf((p: mutable.WrappedArray[Double]) => Vectors.dense(p.toArray[Double]))
-    val rdd = defineLabelValues(spark, data)
-    spark.createDataFrame(rdd).toDF("label", "values")
-      .withColumn("features", defineFeatures(col("values")))
-      .select("label", "features")
+    if(option == "train"){
+      val rdd = defineTrainLabelValues(spark, data)
+      trainLabelFeatures = spark.createDataFrame(rdd).toDF(idColumn, labelColumn, "values")
+        .withColumn(featureColumn, defineFeatures(col("values")))
+        .select(idColumn, labelColumn, featureColumn)
+    } else if(option == "test"){
+      val rdd = defineTestLabelValues(spark, data)
+      testLabelFeatures = spark.createDataFrame(rdd).toDF(idColumn, "values")
+        .withColumn(featureColumn, defineFeatures(col("values")))
+        .select(idColumn, featureColumn)
+    }
+    this
   }
 
-  def ChiSquareSelector(data: DataFrame): DataFrame = {
-    val chiSelector = new ChiSqSelector().setSelectorType("fpr").setFpr(alpha).setLabelCol("label").setFeaturesCol("features").setOutputCol("selectedFeatures")
-    chiSelector.fit(data).transform(data)
+  def defineChiSquareSelector(): ChiSqSelector = {
+    new ChiSqSelector().setSelectorType("fpr").setFpr(alpha).setLabelCol(labelColumn).setFeaturesCol(featureColumn).setOutputCol(s"$featureColumn-Selected")
   }
+
+  def fit(data: DataFrame): ChiSquareTask = {
+    model = defineChiSquareSelector.fit(trainLabelFeatures)
+    this
+  }
+
+
+  def transform(data: DataFrame, option: String): DataFrame = {
+    if(option == "train"){model.transform(data).select(col(idColumn) , col(labelColumn), col(s"$featureColumn-Selected").alias(featureColumn))}
+    else {model.transform(data).select(col(idColumn), col(s"$featureColumn-Selected").alias(featureColumn))}
+  }
+
+  def getLabelFeatures(option: String): DataFrame = {
+    if(option == "train") {transform(trainLabelFeatures, option)} else {transform(testLabelFeatures, option)}
+  }
+
+
+
 
 }
