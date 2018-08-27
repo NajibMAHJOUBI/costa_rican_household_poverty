aae07f107031691c8f2af6024faffa2b907a020f
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-08-27 16:09:41 +0200

    complete test with approximation neighbors

--- src/test/scala/fr/poverty/spark/classification/task/KNeighborsClassifier/MinHashLSHTest.scala
+++ src/test/scala/fr/poverty/spark/classification/task/KNeighborsClassifier/MinHashLSHTest.scala
@@ -1,13 +1,13 @@
-package fr.poverty.spark.nearestNeighborSearch
+package fr.poverty.spark.classification.task.KNeighborsClassifier
 
 import org.apache.log4j.{Level, LogManager}
+import org.apache.spark.ml.feature.MinHashLSHModel
 import org.apache.spark.ml.linalg.SQLDataTypes.VectorType
 import org.apache.spark.ml.linalg.SparseVector
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.junit.{After, Before, Test}
 import org.scalatest.junit.AssertionsForJUnit
-
 
 /**
   * Created by mahjoubi on 12/06/18.
@@ -17,6 +17,9 @@ class MinHashLSHTest extends AssertionsForJUnit {
 
   private var spark: SparkSession = _
   private var dfA: DataFrame = _
+  private val inputColumn: String = "features"
+  private val outputColumn: String = "hashes"
+  private val distanceColumn: String = "jaccardDistance"
 
   @Before def beforeAll() {
     spark = SparkSession.builder.master("local")
@@ -37,12 +40,26 @@ class MinHashLSHTest extends AssertionsForJUnit {
   }
 
   @Test def testMinLSH(): Unit = {
-    val minLSH = new MinHashLSHTask("features", "hashes", 5)
+    val minLSH = new MinHashLSHTask(inputColumn, outputColumn, 5, distanceColumn)
     minLSH.defineEstimator()
     minLSH.fit(dfA)
-
-    minLSH.model.transform(dfA).show()
+    assert(minLSH.model.getInputCol == inputColumn)
+    assert(minLSH.model.getOutputCol == outputColumn)
+    assert(minLSH.model.isInstanceOf[MinHashLSHModel])
   }
+
+  @Test def testApproxSimilarityJoin(): Unit = {
+    val minLSH = new MinHashLSHTask(inputColumn, outputColumn, 5, distanceColumn)
+    minLSH.defineEstimator()
+    minLSH.fit(dfA)
+    val similarityJoin = minLSH.approxSimilarityJoin(dfA, dfA)
+    assert(similarityJoin.isInstanceOf[DataFrame])
+    assert(similarityJoin.columns.length == 3)
+    List("idA", "idB", distanceColumn).foreach(column => assert(similarityJoin.columns.contains(column)))
+    assert(similarityJoin.count() == math.pow(dfA.count(), 2))
+  }
+
+
 
 
   @After def afterAll() {
