c143dcba76af324ae6e1393408b0552b1509295f
ubuntu <najib.mahjoubi@gmail.com>
2018-08-30 00:31:06 +0200

    remove modif pythonpath

--- src/main/python/script/test/test_classification/test_logistic_regression.py
+++ src/main/python/script/test/test_classification/test_logistic_regression.py
@@ -1,9 +1,9 @@
 # coding: utf-8
-import sys
-sys.path.append("/home/mahjoubi/Documents/github/costa_rican_household_poverty/src/main/python/script")
 
 import unittest
+
 from sklearn import datasets
+
 from classification.logistic_regression import LogisticRegressionTask
 
 
