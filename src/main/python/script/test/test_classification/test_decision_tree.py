8b8db00242b76087878af89bd0d481f3b99e19ee
ubuntu <najib.mahjoubi@gmail.com>
2018-08-30 00:29:07 +0200

    modif variable name

--- src/main/python/script/test/test_classification/test_decision_tree.py
+++ src/main/python/script/test/test_classification/test_decision_tree.py
@@ -1,9 +1,9 @@
 # coding: utf-8
-import sys
-sys.path.append("/home/mahjoubi/Documents/github/costa_rican_household_poverty/src/main/python/script")
 
 import unittest
+
 from sklearn import datasets
+
 from classification.decision_tree import DecisionTreeTask
 
 
@@ -12,10 +12,10 @@ class TestDecisionTreeTask(unittest.TestCase):
     def test_decision_tree(self):
         iris = datasets.load_iris()
         X, y = iris.data[:, :2], iris.target
-        decision_tree = DecisionTreeTask()
-        decision_tree.define_estimator()
-        decision_tree.fit(X, y)
-        prediction = decision_tree.predict(X)
+        classifier = DecisionTreeTask()
+        classifier.define_estimator()
+        classifier.fit(X, y)
+        prediction = classifier.predict(X)
 
         self.assertEqual(prediction.shape[0], y.shape[0])
 
