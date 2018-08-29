c47e8011bcd83a1585a613de927d34be0267c87f
ubuntu <najib.mahjoubi@gmail.com>
2018-08-30 00:30:20 +0200

    remove mod pythonpath + name variables

--- src/main/python/script/test/test_classification/test_gaussian_nb.py
+++ src/main/python/script/test/test_classification/test_gaussian_nb.py
@@ -1,11 +1,9 @@
 # coding: utf-8
 
-import sys
 import unittest
 
 from sklearn import datasets
 
-sys.path.append("/home/mahjoubi/Documents/github/costa_rican_household_poverty/src/main/python/script")
 from classification.gaussian_nb import GaussianNBTask
 
 
@@ -14,10 +12,10 @@ class TestGaussianNBTask(unittest.TestCase):
     def test_gaussian_nb(self):
         iris = datasets.load_iris()
         X, y = iris.data[:, :2], iris.target
-        gaussian_nb = GaussianNBTask()
-        gaussian_nb.define_estimator()
-        gaussian_nb.fit(X, y)
-        prediction = gaussian_nb.predict(X)
+        classifier = GaussianNBTask()
+        classifier.define_estimator()
+        classifier.fit(X, y)
+        prediction = classifier.predict(X)
 
         self.assertEqual(prediction.shape[0], y.shape[0])
 
