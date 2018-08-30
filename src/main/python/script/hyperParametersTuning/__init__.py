8b0547679859657150413748f2251d280535aa93
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-08-30 15:42:08 +0200

    modif PYTHONPATH

--- src/main/python/script/hyperParametersTuning/__init__.py
+++ src/main/python/script/hyperParametersTuning/__init__.py
@@ -1,4 +1,3 @@
-# coding: utf-8
-
-# import sys
-# sys.path.append("/home/mahjoubi/Documents/github/costa_rican_household_poverty/src/main/python/script")
+import os
+import sys
+sys.path.append(os.path.realpath('..'))
