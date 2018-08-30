c91c2553cbc81c873e4aec9ba8534ba5b60af4cb
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-08-30 15:41:47 +0200

    add input parameters kernel,C,degree,gamma

--- src/main/python/script/classification/svc.py
+++ src/main/python/script/classification/svc.py
@@ -3,18 +3,30 @@ from sklearn.svm import SVC
 
 from classifier_task import ClassifierTask
 
+defaults_parameters = SVC().get_params()
+
 
 class SVCTask(ClassifierTask):
 
-    def __init__(self):
+    def __init__(self, kernel=defaults_parameters["kernel"],
+                 C=defaults_parameters["C"],
+                 degree=defaults_parameters["degree"],
+                 gamma=defaults_parameters["gamma"]):
         ClassifierTask.__init__(self)
+        self.__kernel__ = kernel
+        self.__C__ = C
+        self.__degree__ = degree
+        self.__gamma__ = gamma
 
     def __str__(self):
-        s = "SVCClassifier"
+        s = "SVC Classifier"
         return s
 
     def define_estimator(self):
-        self.estimator = SVC()
+        self.estimator = SVC(C=self.__C__,
+                             kernel=self.__kernel__,
+                             degree=self.__degree__,
+                             gamma=self.__gamma__)
 
 
 if __name__ == "__main__":
