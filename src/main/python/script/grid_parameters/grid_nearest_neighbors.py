402bc000b484fbbf355765a71a96ad18a42340d0
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-08-31 17:48:34 +0200

    input data metric

--- src/main/python/script/grid_parameters/grid_nearest_neighbors.py
+++ src/main/python/script/grid_parameters/grid_nearest_neighbors.py
@@ -1,7 +1,7 @@
 
 def get_grid_parameters():
-    return [{"n_neighbors": [3, 5, 7, 9],
-             "weights": ["uniform", "distance"],
-             "algorithm": ["ball_tree", "kd_tree", "brute"],
-             "leaf_size": [10, 20, 30],
-             "p": [1, 2]}]
+    return [{"n_neighbors": [1, 5, 10, 15], "weights": ["uniform", "distance"],
+             "algorithm": ["ball_tree"], "leaf_size": [10, 20, 30],
+             "metric": ["jaccard"]},
+            {"n_neighbors": [1, 5, 10, 15], "weights": ["uniform", "distance"],
+             "algorithm": ["brute"], "metric": ["jaccard"], "leaf_size": [30]}]
