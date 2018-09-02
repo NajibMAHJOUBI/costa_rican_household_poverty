dbc579e547fe079d6031daad2b359352a278b8b4
ubuntu <najib.mahjoubi@gmail.com>
2018-09-02 16:19:14 +0200

    new assert on types

--- src/main/python/script/test/test_grid_parameters/test_grid_decision_tree.py
+++ src/main/python/script/test/test_grid_parameters/test_grid_decision_tree.py
@@ -11,11 +11,9 @@ class TestGridParametersDecisionTreeTask(unittest.TestCase):
     def test_grid_parameters(self):
         grid = grid_decision_tree.get_grid_parameters()
         self.assertTrue(isinstance(grid, types.ListType))
-        parameters_decision_tree = ["criterion", "splitter", "max_depth", "max_features"]
 
         for dic_parameters in grid:
-            for value in dic_parameters.keys():
-                self.assertTrue(value in parameters_decision_tree)
+            self.assertTrue(isinstance(dic_parameters, types.DictionaryType))
 
 
 if __name__ == '__main__':
