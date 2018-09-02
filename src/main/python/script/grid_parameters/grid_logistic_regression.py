d66ee5f9d7772d26cda82e2980ed09590ef01d12
ubuntu <najib.mahjoubi@gmail.com>
2018-09-02 16:59:49 +0200

    changed parameters tested

--- src/main/python/script/grid_parameters/grid_logistic_regression.py
+++ src/main/python/script/grid_parameters/grid_logistic_regression.py
@@ -1,3 +1,12 @@
 
 def get_grid_parameters():
-    pass
+    return [
+        {"penalty": ["l2"],
+         "dual": [True, False],
+         "C": [100.0, 10.0, 1.0, 0.1, 0.01],
+         "fit_intercept": [True, False]},
+        {"penalty": ["l1"],
+         "dual": [True, False],
+         "C": [1e-7, 4.0, 2, 4.0/3.0, 1.0],
+         "fit_intercept": [True, False]},
+    ]
