2adb2a87c3b0d3faaa94f28e863ac3d4ca55e2e2
ubuntu <najib.mahjoubi@gmail.com>
2018-09-04 22:07:27 +0200

    add learning_rate parameter

--- src/main/python/script/grid_parameters/grid_xgboost.py
+++ src/main/python/script/grid_parameters/grid_xgboost.py
@@ -1,3 +1,5 @@
 
 def get_grid_parameters():
-    return [{'max_depth': [5, 10, 15, 20, 25, 30], 'n_estimators': [50, 100, 200]}]
+    return [{'max_depth': [5, 10, 15, 20, 25, 30],
+             'n_estimators': [100, 200, 300, 400, 500],
+             'learning_rate': [0.0001, 0.001, 0.01, 0.1]}]
