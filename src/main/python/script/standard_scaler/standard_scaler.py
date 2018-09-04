65aa598f13bba7c4dad92624f669bd17c5bbf313
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-09-04 17:36:39 +0200

    complete test

--- src/main/python/script/standard_scaler/standard_scaler.py
+++ src/main/python/script/standard_scaler/standard_scaler.py
@@ -6,16 +6,20 @@ class StandardScalerTask:
 
     def __init__(self, data):
         self.data = data
+        self.estimator = None
+        self.model = None
 
     def __str__(self):
         s = "Standard Scaler Task"
         return s
 
     def define_estimator(self):
-        self.estimator = StandardScaler(copy=True, with_mean=True, with_std=True)
+        self.estimator = StandardScaler(copy=True,
+                                        with_mean=True,
+                                        with_std=True)
 
     def fit(self):
         self.model = self.estimator.fit(self.data)
 
-    def transform(self):
-        return self.model.transform(self.data)
+    def transform(self, data):
+        return self.model.transform(data)
