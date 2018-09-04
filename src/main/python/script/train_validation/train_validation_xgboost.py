e1e379262c143fdb2a00b5b76253abec1338490a
ubuntu <najib.mahjoubi@gmail.com>
2018-09-04 22:08:08 +0200

    add learning_rate parameter

--- src/main/python/script/train_validation/train_validation_xgboost.py
+++ src/main/python/script/train_validation/train_validation_xgboost.py
@@ -26,20 +26,21 @@ class TrainValidationXGBoost:
         file_results.write("index,accuracy,precision,recall,f1\n")
 
         file_classifier = open(os.path.join(self.__save_path__, "classifier.csv"), "a+")
-        file_classifier.write("index,max_depth,n_estimators\n")
+        file_classifier.write("index,max_depth,n_estimators,learning_rate\n")
 
         # 'max_depth': [2, 4, 6], 'n_estimators'
         index = 0
         for params in get_grid_parameters():
-            for max_depth, n_estimators in product(params["max_depth"], params["n_estimators"]):
+            for max_depth, n_estimators, learning_rate in product(params["max_depth"], params["n_estimators"], params["learning_rate"]):
                 # print("{0},{1}\n".format(index, estimator))
                 classifier = XGBoostClassifierTask(max_depth=max_depth,
-                                                   n_estimators=n_estimators)
+                                                   n_estimators=n_estimators,
+                                                   learning_rate=learning_rate)
                 classifier.define_estimator()
                 classifier.fit(self.__X_train__, self.__y_train__)
                 prediction = classifier.predict(self.__X_validation__)
                 accuracy, precision, recall, f1 = all_scores.all_scores(self.__y_validation__, prediction, "macro")
-                file_classifier.write("{0},{1},{2}\n".format(index, max_depth, n_estimators))
+                file_classifier.write("{0},{1},{2},{3}\n".format(index, max_depth, n_estimators, learning_rate))
                 file_results.write("{0},{1},{2},{3},{4}\n".format(index, accuracy, precision, recall, f1))
                 index += 1
 
