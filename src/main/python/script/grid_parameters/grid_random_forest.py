df987c827cde4bdbb4bea6c5eb28c90b0485f5a3
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-09-06 13:03:11 +0200

    redefine range parameters

--- src/main/python/script/grid_parameters/grid_random_forest.py
+++ src/main/python/script/grid_parameters/grid_random_forest.py
@@ -1,7 +1,7 @@
 
 def get_grid_parameters():
-    return [{"n_estimators": [50, 100, 150],
+    return [{"n_estimators": [100, 200, 300, 400, 500],
              "criterion": ["gini"],
-             "max_depth": [5, 10, 15],
-             "min_samples_split": [5, 10, 15],
-             "min_samples_leaf": [5, 10, 15]}]
+             "max_depth": [1, 10, 20, 30],
+             "min_samples_split": [2, 3, 4, 5],
+             "min_samples_leaf": [2, 3, 4, 5]}]
