0c8e8d6cbba8e1083b3e050e247535b77be952e9
Najib Mahjoubi <najib.mahjoubi@seniorplanet.fr>
2018-09-06 13:25:51 +0200

    cv for random forest

--- src/main/python/script/grid_search/grid_search_cv.py
+++ src/main/python/script/grid_search/grid_search_cv.py
@@ -1,34 +1,34 @@
 # coding: utf-8
-import sys
-sys.path.append("/home/mahjoubi/Documents/github/costa_rican_household_poverty/src/main/python/script")
 
 from classification.classifier_task import ClassifierTask
-from classification.nearest_neighbors import KNeighborsClassifierTask
 from classification.decision_tree import DecisionTreeTask
-from classification.random_forest import RandomForestTask
 from classification.gaussian_nb import GaussianNBTask
 from classification.logistic_regression import LogisticRegressionTask
+from classification.nearest_neighbors import KNeighborsClassifierTask
+from classification.random_forest import RandomForestTask
 from grid_parameters import grid_decision_tree
-from grid_parameters import grid_random_forest
-from grid_parameters import grid_nearest_neighbors
 from grid_parameters import grid_gaussian_nb
 from grid_parameters import grid_logistic_regression
+from grid_parameters import grid_nearest_neighbors
+from grid_parameters import grid_random_forest
 from sklearn.model_selection import GridSearchCV
 
 
 class GridSearchCVTask(ClassifierTask):
 
-    def __init__(self, number_folds, estimator_algorithm):
+    def __init__(self, number_folds, base_estimator, scoring):
         ClassifierTask.__init__(self)
         self.number_folds = number_folds
-        self.estimator_algorithm = estimator_algorithm
+        self.base_estimator = base_estimator
+        self.scoring = scoring
 
     def __str__(self):
         s = "Number of folds: {0}\n".format(self.number_folds)
-        s += "Estimator algorithm: {0}\n".format(self.estimator_algorithm)
+        s += "Base estimator: {0}\n".format(self.base_estimator)
+        s += "Scoring: {0}\n".format(self.scoring)
         return s
 
-    def define_estimator(self):
+    def define_base_estimator(self):
         classifier = None
         if self.estimator_algorithm == "decision_tree":
             classifier = DecisionTreeTask()
@@ -38,16 +38,17 @@ class GridSearchCVTask(ClassifierTask):
             classifier = LogisticRegressionTask()
         elif self.estimator_algorithm == "k_nearest_neighbors":
             classifier = KNeighborsClassifierTask()
-        elif self.estimator_algorithm == "all_features":
+        elif self.estimator_algorithm == "random_forest":
             classifier = RandomForestTask()
 
         classifier.define_estimator()
         return classifier.estimator
 
-    def grid_search_cv(self):
-        self.estimator = GridSearchCV(self.define_estimator(),
+    def estimator(self):
+        self.estimator = GridSearchCV(estimator=self.define_estimator(),
                                       cv=self.number_folds,
                                       param_grid=self.grid_parameters(),
+                                      scoring=self.scoring,
                                       verbose=1)
 
     def grid_parameters(self):
@@ -59,25 +60,5 @@ class GridSearchCVTask(ClassifierTask):
             return grid_logistic_regression.get_grid_parameters()
         elif self.estimator_algorithm == "k_nearest_neighbors":
             return grid_nearest_neighbors.get_grid_parameters()
-        elif self.estimator_algorithm == "all_features":
+        elif self.estimator_algorithm == "random_forest":
             return grid_random_forest.get_grid_parameters()
-
-    def fit(self, X, y):
-        self.model = self.estimator.fit(X, y)
-
-    def predict(self, X):
-        return self.model.predict(X)
-
-
-if __name__ == "__main__":
-    from sklearn import datasets
-    iris = datasets.load_iris()
-    X = iris.data[:, :2]  # we only take the first two features.
-    y = iris.target
-
-    estimator_algorithms = ["gaussian_nb"]  # "k_nearest_neighbors", "decision_tree", "all_features"
-    for estimator_algorithm in estimator_algorithms:
-        print("Estimator algorithm: {0}".format(estimator_algorithm))
-        search_cv = GridSearchCVTask(3, estimator_algorithm)
-        search_cv.grid_search_cv()
-        search_cv.fit(X, y)
