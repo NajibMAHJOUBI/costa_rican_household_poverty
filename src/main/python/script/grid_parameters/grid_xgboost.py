c058f02da4c81232eb8d2b5016d04424efe81bf0
ubuntu <najib.mahjoubi@gmail.com>
2018-09-06 22:24:25 +0200

    modif range parameter

--- src/main/python/script/grid_parameters/grid_xgboost.py
+++ src/main/python/script/grid_parameters/grid_xgboost.py
@@ -1,5 +1,5 @@
 
 def get_grid_parameters():
-    return [{'max_depth': [5, 10, 15, 20, 25, 30],
-             'n_estimators': [100, 200, 300, 400, 500],
+    return [{'max_depth': [10, 20, 30],
+             'n_estimators': [100, 200, 300],
              'learning_rate': [0.0001, 0.001, 0.01, 0.1]}]
